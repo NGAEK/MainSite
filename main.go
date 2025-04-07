872410925b15a4f4286ce8891d7856b1503d60c8
ImCluzzy <imcluzzy@gmail.com>
2025-04-07 23:37:51 +0300

    commit

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"net/url"
+)
 
+func main() {
+	request := http.Request{
+		Method: "GET",
+		URL: &url.URL{
+			Scheme: "http",
+			Host:   "localhost:8080",
+			Path:   "/",
+		},
+	}
 }
